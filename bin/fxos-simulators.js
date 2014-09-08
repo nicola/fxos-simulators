181b348b59bc5e71ae24db06ec72470185bee867
nicolagreco <notsecurity@gmail.com>
2014-09-08 10:41:41 -0700

    adding cli-table support

--- bin/fxos-simulators.js
+++ bin/fxos-simulators.js
@@ -2,12 +2,29 @@
 
 var fs = require('fs');
 var path = require('path');
+var Table = require('cli-table');
 var findSimulators = require('../index');
+
+function getUserHome() {
+  return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+}
+
+var table = new Table({
+  chars: { 'top': '' , 'top-mid': '' , 'top-left': '' , 'top-right': ''
+         , 'bottom': '' , 'bottom-mid': '' , 'bottom-left': '' , 'bottom-right': ''
+         , 'left': '' , 'left-mid': '' , 'mid': '' , 'mid-mid': ''
+         , 'right': '' , 'right-mid': '' , 'middle': ' ' },
+  style: { 'padding-left': 0, 'padding-right': 4 }
+});
 
 var opts = require("nomnom")
   .option('sdk', {
     help: 'Version of FirefoxOS',
     metavar: '<sdk version>'
+  })
+  .option('json', {
+    flag: true,
+    help: 'Formats in json'
   })
   .option('version', {
     flag: true,
@@ -23,5 +40,15 @@ var opts = require("nomnom")
 if (opts.version) return;
 
 findSimulators(opts, function(err, b2gs){
-  console.log(b2gs);
+  if (opts.json) return console.log(b2gs);
+
+  var header = ['SDK', 'BIN'];
+  table.push(header);
+
+  b2gs.forEach(function(b2g) {
+    var row = [b2g.sdk, b2g.bin.replace(getUserHome(), '~')];
+    table.push(row);
+  });
+
+  console.log(table.toString());
 });
